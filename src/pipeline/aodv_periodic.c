47490abafe29f581de89c04be022ff3e1906188e
Christian Mehlis <mehlis@inf.fu-berlin.de>
2011-08-06 11:06:24 +0200

    change debug

--- src/pipeline/aodv_periodic.c
+++ src/pipeline/aodv_periodic.c
@@ -40,6 +40,7 @@ dessert_per_result_t aodv_periodic_send_rreq(void* data, struct timeval* schedul
 
     aodv_link_break_element_t* dest, *tmp;
     DL_FOREACH_SAFE(head, dest, tmp) {
+        dessert_info("periodic send rreq to: " MAC, EXPLODE_ARRAY6(dest->host));
         aodv_send_rreq(dest->host, &timestamp, NULL, 0, 0);
     }
     return DESSERT_PER_KEEP;
